b230887b2b7321433bef0b39c400539b052c7deb
Sandblaze05 <tejaschauhan0512@gmail.com>
2024-04-21 11:19:10 +0530

    more

--- amazon_clone/script.js
+++ amazon_clone/script.js
@@ -1,38 +1,56 @@
-const imgs=document.querySelectorAll('.header-slider ul img');
-const prev_btn=document.querySelector('.control-prev');
-const next_btn=document.querySelector('.control-next');
-let n=0;
+const imgs = document.querySelectorAll('.header-slider ul img');
+const prev_btn = document.querySelector('.control-prev');
+const next_btn = document.querySelector('.control-next');
+let n = 0;
 function changeSlider() {
-    for(let i=0;i<imgs.length;i++){
-        imgs[i].style.display='none';
+    for (let i = 0; i < imgs.length; i++) {
+        imgs[i].style.display = 'none';
     }
-    imgs[n].style.display='block';
+    imgs[n].style.display = 'block';
 }
 changeSlider();
-prev_btn.addEventListener('click',(e)=>{
-    if(n>0){
+prev_btn.addEventListener('click', (e) => {
+    if (n > 0) {
         n--;
     }
-    else{
-        n=imgs.length-1;
+    else {
+        n = imgs.length - 1;
     }
     changeSlider();
 });
-next_btn.addEventListener('click',(e)=>{
-    if(n<imgs.length-1){
+next_btn.addEventListener('click', (e) => {
+    if (n < imgs.length - 1) {
         n++;
     }
-    else{
-        n=0;
+    else {
+        n = 0;
     }
     changeSlider();
 });
 
 
-const scrollContainer=document.querySelectorAll('.products');
-for(const item of scrollContainer){
-    item.addEventListener('wheel',(evt)=>{
+const scrollContainer = document.querySelectorAll('.products');
+for (const item of scrollContainer) {
+    item.addEventListener('wheel', (evt) => {
         evt.preventDefault();
-        item.scrollLeft+=evt.deltaY;
+        item.scrollLeft += evt.deltaY;
     })
+}
+
+let c=0
+function myFunction(){
+    let x=document.getElementById('menu');
+    if(c==0){
+        x.style.display='inline';
+        c=1;
+    }
+    else{
+        x.style.display='none';
+        c=0;
+    }
+}
+
+function toDeal(){
+    const e=document.getElementById('deals');
+    e.scrollIntoView();
 }
